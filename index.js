d8b8f307ba9a75c8b0d65f09641d75b04a75bbac
Bryan A Quero <bryanq2008@live.com>
2022-11-29 20:05:19 -0500

    update 05 index.js

--- index.js
+++ index.js
@@ -1,56 +1,42 @@
-const inquirer = require('inquirer');
 const fs = require('fs');
-const { default: Choice } = require('inquirer/lib/objects/choice');
 
-const generateMarkdown = ({ name, description, tableOf, hyperLink }) => `
-# ${YourProjectTitle}
----
-## Table of Contents
----
-    -${tableOf}
-    
-## Project Description
----
+import inquirer from 'inquirer';
 
+import { default as Choice } from 'inquirer/lib/objects/choice';
 
-
-## Website project link
----
-[Link here](${hyperLink})
-
-`;
+const generateMarkdown = ({ name, description, tableOf, hyperLink }) => '\n# ' + name + ' \n---\n## Table of Contents\n---\n    -' + tableOf + '\n    \n## Project Description\n--- ' + description + '\n\n\n\n## Website project link\n---\n[Link here](' + hyperLink + ')\n\n';
 
 inquirer.prompt([
-        {
-            name: 'title',
-            message: 'Name title of your Project?',
-            type: 'input'
-        },
-        {
-            desc:'description',
-            message: 'Description of the project',
-            type:'input'
-        },
-        {
-            tableOf:'contents',
-            message: 'Table of Contents of your project',
-            type:'checkbox',
-            choices:['description','Installation','Usage', 'Credits','License'],
+    {
+        name: 'title',
+        message: 'Name title of your Project?',
+        type: 'input'
+    },
+    {
+        desc: 'description',
+        message: 'Description of the project',
+        type: 'input'
+    },
+    {
+        tableOf: 'contents',
+        message: 'Table of Contents of your project',
+        type: 'checkbox',
+        choices: ['description', 'Installation', 'Usage', 'Credits', 'License']
 
-        },
-        {
-            hyperLink:'webSite',
-            message: 'Input your website link project',
-            type:'input'
-        },
+    },
+    {
+        hyperLink: 'webSite',
+        message: 'Input your website link project',
+        type: 'input'
+    },
 
 
-    ])
+])
 
     .then((answers) => {
         const markdownPageContent = generateMarkdown(answers);
 
-        fs.writeFile('./DS_Store/README.md', markdownPageContent, (err) =>
+       fs.writeFile('./DS_Store/README.md', markdownPageContent, (err) =>
             err ? console.log(err) : console.log('Successfully created README.md')
         );
     });
