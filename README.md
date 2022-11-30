605a16b9aebd5a72543649971a9f8cb5c17b1cfe
Bryan A Quero <bryanq2008@live.com>
2022-11-29 19:05:01 -0500

    project update

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 ## Description
 
-README.md Generator app the user quickly and easily create a README file by using a command-line application to generate one. This allows the project creator to devote more time to working on the project.
+README.md Generator app the user quickly and easily can create a README file by using a command-line application to generate one. This allows the project creator to devote more time to working on the project.
 
 
--- index.js
+++ index.js
@@ -1,36 +1,52 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const generateMarkdown = ({ YourProjectTitle, description, motivation, build, hyperLink }) => `
+const generateMarkdown = ({ name, description, tableOf, hyperLink }) => `
 # ${YourProjectTitle}
 ---
 
 ## Project Description
 ---
 
+## Table of Contents
+---
 
+
+## Website project link
+---
 [Link here](${hyperLink})
 
 `;
 
-inquirer
-    .prompt([
+inquirer.prompt([
         {
-            type: 'input',
-            YourProjectTitle: 'title',
-            message: 'Title of your Project?'
+            name: 'title',
+            message: 'Name title of your Project?',
+            type: 'input'
         },
         {
-            type:'input',
             description:'projDescription',
-            message: 'Description of the project'
-        }
+            message: 'Description of the project',
+            type:'input'
+        },
+        {
+            tableOf:'contents',
+            message: 'Table of Contents',
+            type:'input'
+        },
+        {
+            hyperLink:'webSite',
+            message: 'Type the website link of the project',
+            type:'input'
+        },
+
+
     ])
 
     .then((answers) => {
-        const htmlPageContent = generateMarkdown(answers);
+        const markdownPageContent = generateMarkdown(answers);
 
-        fs.writeFile('README.md', markdownPageContent, (err) =>
+        fs.writeFile('./DS_Store/README.md', markdownPageContent, (err) =>
             err ? console.log(err) : console.log('Successfully created README.md')
         );
     });
