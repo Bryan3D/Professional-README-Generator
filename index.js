e60aad5fbf9a1d7c5ca1dd969184db7dba8ee596
Bryan A Quero <bryanq2008@live.com>
2022-11-29 20:09:39 -0500

    update06 index.js

--- index.js
+++ index.js
@@ -1,34 +1,35 @@
 const fs = require('fs');
 
-import inquirer from 'inquirer';
+const inquirer = require('inquirer');
 
 import { default as Choice } from 'inquirer/lib/objects/choice';
 
 const generateMarkdown = ({ name, description, tableOf, hyperLink }) => '\n# ' + name + ' \n---\n## Table of Contents\n---\n    -' + tableOf + '\n    \n## Project Description\n--- ' + description + '\n\n\n\n## Website project link\n---\n[Link here](' + hyperLink + ')\n\n';
 
-inquirer.prompt([
-    {
+inquirer
+    .prompt([
+     {
         name: 'title',
         message: 'Name title of your Project?',
         type: 'input'
-    },
-    {
+     },
+     {
         desc: 'description',
         message: 'Description of the project',
         type: 'input'
-    },
-    {
+        },
+        {
         tableOf: 'contents',
         message: 'Table of Contents of your project',
         type: 'checkbox',
         choices: ['description', 'Installation', 'Usage', 'Credits', 'License']
 
-    },
-    {
+        },
+        {
         hyperLink: 'webSite',
         message: 'Input your website link project',
         type: 'input'
-    },
+        },
 
 
 ])
